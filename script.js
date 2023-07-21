3262661a605d12a1430e8aaf607b31ea38e77e82
sofia baezzato <baezzatosofia331gmail.com>
2023-07-21 18:08:56 +0200

    add logic and basic UI

---
+++ script.js
@@ -0,0 +1,74 @@
+const DEFAULT_SIZE = 16;
+const DEFAULT_MODE = 'classic';
+
+let size = DEFAULT_SIZE;
+let mode = DEFAULT_MODE;
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+const sizeLabel = document.getElementById('sizeLabel')
+const sizeSlider = document.getElementById('slider');
+const modeSelector = document.getElementById('modeSelector')
+const eraserBtn = document.getElementById('eraserBtn');
+const clearBtn = document.getElementById('clearBtn')
+const grid = document.getElementById('grid');
+
+sizeSlider.addEventListener("change", (e) => {
+    size = e.target.value
+    reloadGrid();
+})
+
+sizeSlider.addEventListener("input", (e) => {
+    inputSize = e.target.value;
+    sizeLabel.innerHTML = "Canvas size: " + e.target.value + " x " + e.target.value;
+})
+
+modeSelector.addEventListener("click", () => {
+    mode = modeSelector.options[modeSelector.selectedIndex].value;
+})
+
+clearBtn.onclick = () => reloadGrid();
+
+function draw(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mouseover', changeColor)
+        gridElement.addEventListener('mousedown', changeColor)
+        grid.appendChild(gridElement);
+    }
+}
+
+function clear() {
+    grid.innerHTML = "";
+}
+
+function reloadGrid() {
+    clear();
+    draw(size);
+}
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    if (mode === 'classic') {
+        e.target.style.backgroundColor = 'var(--text-color)';
+    }
+    else if (mode === 'rainbow') {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        e.target.style.backgroundColor = "#" + randomColor;
+    }
+    else if (mode === 'eraser') {
+        e.target.style.backgroundColor = '#fefefe';
+    }
+    
+}
+
+window.onload = () => {
+    draw(DEFAULT_SIZE);
+}
+
